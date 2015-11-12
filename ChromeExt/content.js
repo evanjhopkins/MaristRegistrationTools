0902a6416dfb9b8d129fe4c8d7e0ce66ba436675
popcat <moviemaker7@gmail.com>
2015-11-12 01:17:27 -0500

    now 100% js, also added color coding to ratings

--- ChromeExt/content.js
+++ ChromeExt/content.js
@@ -10,55 +10,77 @@ function fireContentLoadedEvent () {
     if($(".captiontext").text()==="Sections Found"){
       $(".datadisplaytable tr td:nth-child(17)").each(function (i,elem) {
         prof = $(elem).context.innerText;
-        clean_name(prof);
+        var name = clean_name(prof);
+        search_for_prof(name, elem);
       });
-      //lookup();
     }
 }
 
-function lookup(){
-  if (cnt > professors.length-1){
-    return;
-  }
-
-  var elem = professors[cnt];
-  cnt++;
-  var prof = $(elem).context.innerText;
-  if (prof in mem_cache){
-    console.log("mem");
-    rating = mem_cache[prof];
-    $(elem).html("<span style='color:red;'>("+rating+")</span> "+$(elem).context.innerText);
-    lookup();
-  }else{
-    console.log("lookup");
-    chrome.runtime.sendMessage({
+//attempts to search RMP for the prof and get their ID
+function search_for_prof(name, elem){
+  chrome.runtime.sendMessage({
         method: 'GET',
         action: 'xhttp',
-        url: 'http://evanjhopkins.com/MaristCourseLookupProfessorRating/API/ratemyprof.php?prof='+prof,
+        url: 'http://search.mtvnservices.com/typeahead/suggest/?prefix='+name+'&fq=schoolid_s%3A563&siteName=rmp',
         data: ''
     }, function(responseText) {
-        rating = responseText;
-        mem_cache[$(elem).context.innerText] = rating;
-        $(elem).html("<span style='color:red;'>("+rating+")</span> "+$(elem).context.innerText);
-        lookup();
+        obj = JSON.parse(responseText);
+        //if no results were found, show ? in place of rating
+        if(obj['response']['numFound'] < 1){
+          add_rating_to_prof(" ? ", elem)
+        }
+        //if results were found, take best match (first item)
+        var id = obj['response']['docs'][0]['pk_id'];
+        get_rating(id, elem);
     });
-  }
 }
 
-//attempts to search RMP for the prof and get their ID
-function search_for_prof(){
-
+function get_rating(id, elem){
+  chrome.runtime.sendMessage({
+        method: 'GET',
+        action: 'xhttp',
+        url:   'http://www.ratemyprofessors.com/ShowRatings.jsp?tid='+id,
+        data: ''
+    }, function(responseText) {
+        html = $.parseHTML(responseText);
+        var rating = $(html).find(  ":contains('Overall Quality') .breakdown-header .grade")[0].innerText;
+        add_rating_to_prof(rating, elem)
+    });
 }
 
-//cleans the nam displayed on marist course lookup in prep for search
+function add_rating_to_prof(rating, elem){
+    var color = "red";
+    if (rating != " ? "){
+      if (rating >= 4.0){
+        color = "#33cc33";
+      }else if(rating >= 3.0){
+        color = "#00ccff";
+      }else if(rating >= 2.0){
+        color = "#fe7f00";
+      }
+    } 
+    $(elem).html("(<span style='color:"+color+";'>"+rating+"</span>) "+  $(elem).html());
+}
+
+//cleans the name displayed on marist course lookup in prep for search
 // "Matthew P. Johnson (P)" --> "Matthew Johnson"
-function clean_name(var name){
+function clean_name(name){
   //split up words in name so we can evaluate each one
   var exploded_name = name.split(" ");
   //this will contain only the words we deem valid (no special chars, no initials)
   var exploded_clean_name = [];
   //iterate through each word in name
   exploded_name.forEach(function(word) {
-    console.log(word);
+    if(word.length < 2){//initial if only 1 letter
+      return;//reject word
+    }
+    if(word.indexOf("(") > -1){// filtering out "(P)"
+      return;//reject word
+    }
+    //this word has passed all tests, so add it to the cleaned name
+    exploded_clean_name.push(word);
   });
+  //we only want first and last name
+  var clean_name = exploded_clean_name[0]+" "+exploded_clean_name[exploded_clean_name.length-1];
+  return clean_name;
 }
